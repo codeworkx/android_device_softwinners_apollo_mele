1521e8cae22af5e320979acd97d02c290f4ca434
codeworkx <codeworkx@cyanogenmod.org>
2012-12-31 10:01:43 +0100

    specify bootanimation size

--- cm.mk
+++ cm.mk
@@ -20,6 +20,10 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := apollo-mele
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
